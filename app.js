a34fae9b5d7018a86f03d43cf5e3078357f38b2d
saikiran030996 <saikiran.davangams@gmail.com>
2024-01-14 15:30:19 +0100

    Episode-01

---
+++ app.js
@@ -0,0 +1,13 @@
+const parent  = React.createElement(
+    'div',{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},'I am an h1 tag'),React.createElement("h2",{},'I am an h2 tag')]
+), React.createElement('div',{id:'child2'},[React.createElement('h1',{},'I am an h1 tag'),React.createElement("h2",{},'I am an h2 tag')])]); 
+        
+        
+        
+// JSX
+
+
+console.log(parent)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
