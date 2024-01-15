fe224b14e455ba5ea160081068089ee01358cffa
saikiran030996 <saikiran.davangams@gmail.com>
2024-01-15 01:06:40 +0100

    Episode 2 updated

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # :pray: Namaste React
+
+# Parcel
+
+- Dev build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification of file
+- Bundle all the files
+- Compress files
+- Consistency Hashing
+- Code Splitting
+- Differential Bundling - support Older Browers
+- Diagonstic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+import React from 'react';
+import ReactDOM from "react-dom/client"
+
+
+
 const parent  = React.createElement(
-    'div',{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},'I am an h1 tag'),React.createElement("h2",{},'I am an h2 tag')]
+    'div',{id:'parent'},[React.createElement('div',{id:'child'},[React.createElement('h1',{},'This is Namaste React 🚀'),React.createElement("h2",{},'By Saikiran')]
 ), React.createElement('div',{id:'child2'},[React.createElement('h1',{},'I am an h1 tag'),React.createElement("h2",{},'I am an h2 tag')])]); 
         
         
